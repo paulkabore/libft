e6b85dfc126d2c8f045fcbc54efd57e236c177e5
Paul Kabore <paulkabore333@gmail.com>
2024-08-15 08:22:17 +0000

    add ft_memmove.c

--- libft.h
+++ libft.h
@@ -9,6 +9,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
-void	*ft_memcpy(void *dest, const void *src, si    ze_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
