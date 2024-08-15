a64e40f1553caabd7a484ef4093a784459677c51
Paul Kabore <paulkabore333@gmail.com>
2024-08-15 15:47:45 +0000

    add ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,1 +1,24 @@
-/data/data/com.termux/files/usr/bin/bash: line 1: c_formatter_42: command not found
+#include "../libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t i = 0;
+	char *d = (char *)dest;
+	const char *s = (char *)src;
+
+	if (s <= d && s + n >= d)
+	{
+		i = n - 1;
+		while (i >= 0)
+		{
+			*(d + i) = *(s + i);
+			--i;
+		}
+	}
+	while(i < n)
+	{
+		*(d + i) = *(s + i);
+		++i;
+	}
+	return dest;
+}
