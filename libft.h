a68a83c062d23f08cacf83e3ec193014a7fb62b0
Paul Kabore <paulkabore333@gmail.com>
2024-08-16 09:48:22 +0000

    add ft_atoi definition

--- libft.h
+++ libft.h
@@ -10,6 +10,9 @@ char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(void *s, int c, size_t n);
 
+int     ft_atoi(const char *nptr);
 #endif
