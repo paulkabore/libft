074fd80e3a9e393421baaaa6f61ee9b31cb722eb
Paul Kabore <paulkabore333@gmail.com>
2024-08-17 11:32:09 +0000

    add ft_strlcat.c

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,20 @@
+#include "../libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t dsize)
+
+{
+	size_t i = 0;
+
+	if (dsize)
+	{	
+		while (*(dst + i) && ++i);
+		while (i < dsize - 1 && *src)
+		{
+			*(dst + i) = *src++;
+			++i;
+		}
+		*(dst + i) = '\0';
+	}
+	while (*src++ && ++i);
+	return i;
+}
--- libft/main.c
+++ libft/main.c
@@ -7,12 +7,17 @@ const int main(const int argc, const char **argv)
 {
 int i = 1;
 
-char dst[20];
+char dst[40];
+char dst1[40];
+strcpy(dst, "Hello!");
+strcpy(dst1, "Hello!");
+dst[6] = '\0';
+dst1[6] = '\0';
 
 while (i < argc)
 {
-	printf("%zu\t %s %zu - ", ft_strlcpy(dst, argv[i], 20), dst, ft_strlen(dst));
-	printf("%zu\t %s %zu - ", strlcpy(dst, argv[i], 20), dst, ft_strlen(dst));
+	printf("%zu %s %zu", ft_strlcat(dst, argv[i], 40), dst, ft_strlen(dst));
+	printf("%zu %s %zu", strlcat(dst1, argv[i], 40), dst1, ft_strlen(dst1));
 	++i;
 }
 printf("\n");
